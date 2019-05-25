2865be998f3f048cb1d2d929fd4695e004a9f74e
Emil S <42509703+ninjaspl0it@users.noreply.github.com>
2019-05-26 01:06:59 +0200

    Create ftp.py

---
+++ scripts/scanner/ftp.py
@@ -0,0 +1,19 @@
+from .scanner import ProtocolScanner
+import ftplib
+
+class FTPScanner(ProtocolScanner):
+	def __init__(self, protocolName, portNumber, IPAddress):
+		super(FTPScanner, self).__init__(protocolName, portNumber, IPAddress)
+	#have to finish the function signature. And find out what's host and port
+	def verifyCredentials(self, credentials):
+		try:
+			ftp = ftplib.FTP()
+			ftp.connect(self.IPAddress, self.portNumber)
+			ftp.login(credentials.getUsername(), credentials.getPassword())
+			print(ftp.getwelcome())
+			evidence = ftp.sendcmd("SYST").split(' ')[1]+"-based"
+			ftp.quit()
+			# evidence = "Linux raspberrypi 4.14.69+ #1141 Mon Sep 10 15:13:50 BST 2018 armv6l GNU/Linux"
+			return evidence
+		except:
+			return None

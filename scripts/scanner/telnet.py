c87f204cb0617d3ed97e9e3270ae09187752c6a4
Emil S <42509703+ninjaspl0it@users.noreply.github.com>
2019-05-26 01:06:47 +0200

    Create telnet.py

---
+++ scripts/scanner/telnet.py
@@ -0,0 +1,28 @@
+import telnetlib
+from .scanner import ProtocolScanner
+import socket
+
+
+class TelnetScanner(ProtocolScanner):
+
+    def __init__(self, protocolName, portNumber, IPAddress):
+        super(TelnetScanner, self).__init__(protocolName, portNumber, IPAddress)
+
+    def verifyCredentials(self, credentials):
+        pswd = credentials.getPassword()
+        try:
+            telnet = telnetlib.Telnet(str(self.IPAddress), timeout = 2)
+            telnet.read_until(b"login: ")
+            telnet.write((credentials.getUsername()).encode('ascii') + b"\n")
+
+            if pswd:
+                telnet.read_until(b"Password: ")
+                telnet.write(pswd.encode('ascii') + b"\n")
+
+            telnet.write(b"exit\n")
+            evidence = (telnet.read_all().decode('ascii')).split('\n')[3]
+            return evidence
+
+        except Exception as e:
+            print("Exception in telnet: ", str(e))
+            return None

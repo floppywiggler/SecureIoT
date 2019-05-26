7f4c2a262a103fd86bc4d5905a964b50ba5f74f1
Emil S <42509703+ninjaspl0it@users.noreply.github.com>
2019-05-26 09:11:48 +0200

    Create scanner.py

---
+++ scripts/scanners/scanner.py
@@ -0,0 +1,27 @@
+import socket
+
+
+class ProtocolScanner(object):
+    def __init__ (self, protocolName, portNumber, IPAddress):
+        self.protocolName = protocolName
+        self.portNumber = portNumber
+        self.IPAddress = IPAddress
+
+    def getProtocolName(self):
+        return self.protocolName
+
+    def getPortNumber(self):
+        return self.portNumber
+
+    def getIPAddress(self):
+        return self.IPAddress
+
+    def isPortOpen(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(3)
+        result = sock.connect_ex((str(self.IPAddress), self.portNumber))
+        try:
+            sock.shutdown(2)
+        except: # anything really
+            pass
+        return result
